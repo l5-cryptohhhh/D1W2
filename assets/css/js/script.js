4b9083f03cc057a27f60c2319cb32777d5ad9e27
l5-cryptohhhh <nunziatamanuel5@gmail.com>
2026-05-11 11:01:02 +0200

    D1W2

---
+++ assets/css/js/script.js
@@ -0,0 +1,23 @@
+let myfirststring = 'Ciao, Mondo';
+console.log(myfirststring);
+
+let punteggio;
+punteggio = 5;
+console.log(punteggio);
+
+punteggio = 10;
+console.log(punteggio);
+
+const mesi = 12;
+console.log(mesi);
+
+
+{
+console.log(`il numero di mesi è ${mesi}`);
+}
+ 
+
+{
+let studentName = 'Manuel';
+console.log(studentName);
+}
